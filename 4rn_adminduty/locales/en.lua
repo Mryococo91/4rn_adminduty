9e1f19125269c9fc2fda490ed937ae2af3c4098d
halicsarnold <halicsarnold@gmail.com>
2020-11-01 01:35:46 +0100

    Add files via upload

--- 4rn_adminduty/locales/en.lua
+++ 4rn_adminduty/locales/en.lua
@@ -1,1 +1,18 @@
+-----------------------------------------------------
+-- [ESX]AdminDuty | ENGLISH LOCALE (c)4rn_OLD 2020 --
+-----------------------------------------------------
+--
+--	Created by Arnold Halics
+--
+--	This work is licensed under MIT.
+--
+--	If problem this script, please write e-mail: nemerdekel@sohivatal.hu
+--
 
+Locales['en'] = {
+
+	-- 🔻 AdminDuty language tag 🔻
+	['admin_on'] = '✔️ Admin Duty ON! ✔️',
+	['admin_off'] = '❌ Admin Duty OFF! ❌',	
+	['noadmin'] = '❌ You do not have permission!'
+}
---
+++ 4rn_adminduty/locales/hu.lua
@@ -0,0 +1,19 @@
+---------------------------------------------------------
+-- [ESX]AdminDuty | MAGYAR NYELVI FILE (c)4rn_OLD 2020 --
+---------------------------------------------------------
+--
+--	Created by Arnold Halics
+--
+--	This work is licensed under MIT.
+--
+--	If problem this script, please write e-mail: nemerdekel@sohivatal.hu
+--
+
+Locales['hu'] = {
+	
+	-- 🔻 AdminDuty nyelvi cimkék 🔻
+	['admin_on'] = '✔️ Admin szolgálatba léptél! ✔️',
+	['admin_off'] = '❌ Kiléptél Admin szolgálatból! ❌'	,
+	['noadmin'] = '❌ Nincs jogosultságod az admin szolgálathoz!'	
+	
+}
